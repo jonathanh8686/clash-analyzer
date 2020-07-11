8b9b06abf4dec38f3cfbcfe607463e48f102b389
Jonathan Hsieh <jonathanh1386@gmail.com>
2020-07-11 04:34:52 -0700

    Updates

---
+++ precache-manifest.486fe42a188bfd722281e8cad3579232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb81b425bb347b6efa93bd1578ff23e",
+    "url": "/clash-analyzer/index.html"
+  },
+  {
+    "revision": "f2f45534cdacd8aa0b95",
+    "url": "/clash-analyzer/static/css/main.45b83166.chunk.css"
+  },
+  {
+    "revision": "555500ffc48ae9af0f8c",
+    "url": "/clash-analyzer/static/js/2.ea6fc3b9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clash-analyzer/static/js/2.ea6fc3b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f45534cdacd8aa0b95",
+    "url": "/clash-analyzer/static/js/main.98d1da1b.chunk.js"
+  },
+  {
+    "revision": "50849c2cc70b787c02cb",
+    "url": "/clash-analyzer/static/js/runtime-main.a09e6271.js"
+  }
+]);
